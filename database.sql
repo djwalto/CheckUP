21bc241cb757156c1a19674193d3966fd274b5c1
David Walton <davidwalton6772@gmail.com>
2020-08-28 16:09:51 -0500

    added feedback table to database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,17 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "user"
+(
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "feedback"
+(
+    "id" serial primary key,
+    "feeling" INT not null,
+    "understanding" INT not null,
+    "support" INT not null,
+    "comments" text,
+    "flagged" boolean default false,
+    "date" date not null default CURRENT_DATE
 );
