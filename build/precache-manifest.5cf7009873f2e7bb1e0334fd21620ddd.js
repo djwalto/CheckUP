cbc53ababde3837ae1768e5b5fbf70d4f108481a
David Walton <davidwalton6772@gmail.com>
2020-09-24 12:44:29 -0500

    removed /build

---
+++ build/precache-manifest.5cf7009873f2e7bb1e0334fd21620ddd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170488494884ff7939b6b39d8117cbec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "335a9b47a3f1d329aa6e",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "3a1e8d9f0546088b1069",
+    "url": "/static/css/main.ffa52e50.chunk.css"
+  },
+  {
+    "revision": "335a9b47a3f1d329aa6e",
+    "url": "/static/js/2.2b4e8d93.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/static/js/2.2b4e8d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1e8d9f0546088b1069",
+    "url": "/static/js/main.9d9250b6.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
